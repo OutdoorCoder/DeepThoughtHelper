7c8fcd173799332cfcea4254bf957aff65562ab0
Cameron Massey <cameron_massey@yahoo.com>
2018-08-13 21:05:56 -0600

    Database stuff started

--- DeepThoughtHelper.py
+++ DeepThoughtHelper.py
@@ -1,11 +1,11 @@
 #! python3 DeepThoughtHelper.py
 
 # Main program for project
-
+import pymongo
 from getWikiPages import getWikiUrlAddresses
 from searchWikiPages import searchWikiPages
 
-#TODO: Get parameters for getWikiUrlAddresses
+#TODO: Get parameters for getWikiUrlAddresses from some automated thing you're gonna make
 wiki_url_end_address = getWikiUrlAddresses()
 
 # construct those url's above into actual url's
@@ -15,5 +15,9 @@ wiki_url = wiki_url_base + wiki_url_end_address['query']['allpages'][0]['title']
 
 # Search through those wiki pages and find 42!
 print(searchWikiPages(wiki_url))
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
 
 #TODO: Save the results, if any, to the database
