77fa3f61de9d602242416673de39f2573f4dd2f8
Cameron Massey <cameron_massey@yahoo.com>
2018-08-18 10:14:39 -0600

    Now inserts test row into db
    Run mongod from C:\program files\mongodb\server\bin
    
    Then you can connect to the db.

--- DeepThoughtHelper.py
+++ DeepThoughtHelper.py
@@ -14,10 +14,14 @@ wiki_url_base = 'https://en.wikipedia.org/wiki/'
 wiki_url = wiki_url_base + wiki_url_end_address['query']['allpages'][0]['title']
 
 # Search through those wiki pages and find 42!
-print(searchWikiPages(wiki_url))
+results = searchWikiPages(wiki_url)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["mydatabase"]
+collection = mydb['test-collection']
+
+
+dbResult  = collection.insert_one({'text': results, 'url': wiki_url})
 
 #TODO: Save the results, if any, to the database
