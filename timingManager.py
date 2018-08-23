9c2068ac06c6043fce5e43867d741646857670e8
Cameron Massey <cameron_massey@yahoo.com>
2018-08-22 20:48:38 -0600

    A few cosmetic changes

--- timingManager.py
+++ timingManager.py
@@ -1,23 +1,25 @@
 import subprocess
+
 from random import randint
 from time import sleep
+
 from isNowInTimePeriod import isNowInTimePeriod
 
 while True:
 
     #if time is between like 11pm and 6am whatever time I'm in
+    #TODO: Make this actually determine whether the code runs
     if isNowInTimePeriod():
         print(True)
     else:
         print(False)
 
-    #run program
     response = subprocess.run("python DeepThoughtHelper.py")
 
-    #TODO: get this to be a return code, and check if it's zero
     print(response)
 
-    #wait randomized time
-    sleep(randint(10,40))
+    if response.returncode == 1:
+        print("Error has occured")
+        break
 
-    #if fails break
+    sleep(randint(10,40))
